12589180f954db649eec25fe29c852d3c5645571
Patrick <tsinany@hotmail.com>
2018-02-11 21:32:20 -0500

    remove display message activity

--- app/src/main/java/com/robotronix3550/robotronix_scouting_app/MainActivity.java
+++ app/src/main/java/com/robotronix3550/robotronix_scouting_app/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Called when the user taps the Send button */
     /*
     public void sendMessage(View view) {
         // Do something in response to button
